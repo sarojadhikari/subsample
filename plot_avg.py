de9fd2ebc9c3c1c2e68bf7afa3e7f5e33d5739f7
Saroj Adhikari <adh.saroj@psu.edu>
2015-09-09 17:19:13 -0400

    add a seed and xlim

--- plot_avg.py
+++ plot_avg.py
@@ -24,7 +24,7 @@ def figurebox(xs=xsize, ys=ysize):
     plt.figure(num=None, figsize=(xs, ys))
     plt.tick_params(which='both', color="gray")
 
-def PPhideltaL(TYPE="fNL", NG=100, seeds=["7222", "4233", "221192"], subtractG=True, Nmax=512):
+def PPhideltaL(TYPE="fNL", NG=100, seeds=["7222", "4233", "221192", "52210"], subtractG=True, Nmax=512):
     NAME="normal"+TYPE+str(NG)
     for SEED in seeds:
         ssg=SSPowerSpectra(name="normalfNL0", seed=SEED, Nsubs=Nmax)
@@ -46,11 +46,11 @@ def PPhideltaL(TYPE="fNL", NG=100, seeds=["7222", "4233", "221192"], subtractG=T
     kmax=kmin*33
     plt.xlim(kmin, kmax)
     plt.xlabel(r'$k \left( {\rm h/Mpc} \right)$')
-    plt.ylabel(r'$\langle P_{\rm sv}(k) \bar{\delta}_{\rm sv} \rangle$')
+    plt.ylabel(r'$\langle P_{\rm sv}(k) \bar{\delta}_{\rm sv} \rangle/P(k)/\sigma_{\phi_L}^2$')
     plt.title(r'$f_{\rm NL}='+str(NG)+'$', y=1.02)
     plt.show()
     
-def PQeqdeltaL(TYPE="D3", NG=100000, seeds=["7222", "4233", "221192"], subtractG=True, Nmax=512):
+def PQeqdeltaL(TYPE="D3", NG=100000, seeds=["7222", "4233", "221192"], subtractG=True, Nmax=512, factor=1.):
     NAME="normal"+TYPE+str(NG)
     for SEED in seeds:
         ss=SSPowerSpectra(name=NAME, seed=SEED, Nsubs=Nmax)
@@ -65,10 +65,15 @@ def PQeqdeltaL(TYPE="D3", NG=100000, seeds=["7222", "4233", "221192"], subtractG
             Qeqdeltamean = Qeqdeltamean - QeqdeltameanG
         
         Nks=len(Qeqdeltamean)
-        plt.plot(ss.klist[:Nks], Qeqdeltamean)
+        plt.plot(ss.klist[:Nks], factor*Qeqdeltamean)
         
     sqLimitTheory = np.array([np.var(ssg.ds)*NG*2 for i in range(Nks)])
     plt.plot(ss.klist[:Nks], sqLimitTheory, color="black", linestyle="dashed", linewidth=1.5, alpha=0.4)
+    plt.ylabel(r'$\left\langle Q^{\rm equil}(k) \bar{\delta}_{\mathbf{r}_L} \right\rangle$')
+    plt.title(r'$g_{\rm NL}^{\rm T1}='+str(NG)+'$', y=1.02)
+    kmin=2.*np.pi/ss.Lsub
+    kmax=kmin*33
+    plt.xlim(kmin, kmax)
     
     plt.show()
     
