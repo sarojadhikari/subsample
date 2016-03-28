fix print/indent; and add variance options

plot_avg.py
@@ -3,7 +3,7 @@ make useful scripts to plot averaged quantities, averaged over
     * subvolumes,
     * seeds, or
     * scale (k)
-    
+
 quantities like <P_subvolume delta_L> and <Q_eq,L delta_L>
 """
 import matplotlib
@@ -24,7 +24,10 @@ def figurebox(xs=xsize, ys=ysize):
     plt.figure(num=None, figsize=(xs, ys))
     plt.tick_params(which='both', color="gray")
 
-def PPhideltaL(TYPE="fNL", NG=100, seeds=["7222", "4233", "221192", "52210"], subtractG=True, Nmax=512):
+def PPhideltaSqL(TYPE="fNL", NG=100, seeds=["7222", "4233", "221192", "52210"], subtractG=True, Nmax=512):
+    PPhideltaL(TYPE=TYPE, NG=NG, seeds=seeds, subtractG=subtractG, Nmax=Nmax, var=True)
+
+def PPhideltaL(TYPE="fNL", NG=100, seeds=["7222", "4233", "221192", "52210"], subtractG=True, Nmax=512, var=False):
     NAME="normal"+TYPE+str(NG)
     for SEED in seeds:
         ssg=SSPowerSpectra(name="normalfNL0", seed=SEED, Nsubs=Nmax)
@@ -32,16 +35,16 @@ def PPhideltaL(TYPE="fNL", NG=100, seeds=["7222", "4233", "221192", "52210"], su
         sigmasqL=np.var(ssg.ds)
         ss=SSPowerSpectra(name=NAME, seed=SEED, Nsubs=Nmax)
         ss.read_statistics()
-        ss.iBk(sigmasqL)
-        
+        ss.iBk(sigmasqL, variance=var)
+
         iBkmean = ss.iBkmean
-        
+
         if (subtractG):
-            ssg.iBk(sigmasqL)
+            ssg.iBk(sigmasqL, variance=var)
             iBkmean = iBkmean - ssg.iBkmean
-            
+
         plt.plot(ss.klist, iBkmean)
-    
+
     kmin=2.*np.pi/ss.Lsub
     kmax=kmin*33
     plt.xlim(kmin, kmax)
@@ -49,24 +52,24 @@ def PPhideltaL(TYPE="fNL", NG=100, seeds=["7222", "4233", "221192", "52210"], su
     plt.ylabel(r'$\langle P_{\rm sv}(k) \bar{\delta}_{\rm sv} \rangle/P(k)/\sigma_{\phi_L}^2$')
     plt.title(r'$f_{\rm NL}='+str(NG)+'$', y=1.02)
     plt.show()
-    
+
 def PQeqdeltaL(TYPE="D3", NG=100000, seeds=["7222", "4233", "221192"], subtractG=True, Nmax=512, factor=1.):
     NAME="normal"+TYPE+str(NG)
     for SEED in seeds:
         ss=SSPowerSpectra(name=NAME, seed=SEED, Nsubs=Nmax)
         ss.read_statistics()
         Qeqdeltamean = np.mean(ss.fNLeqds, axis=0)
-        
+
         if (subtractG):
             # get the Gaussian piece to subtractG
             ssg=SSPowerSpectra(name="normalfNL0", seed=SEED, Nsubs=Nmax)
             ssg.read_statistics()
             QeqdeltameanG = np.mean(ssg.fNLeqds, axis=0)
             Qeqdeltamean = Qeqdeltamean - QeqdeltameanG
-        
+
         Nks=len(Qeqdeltamean)
         plt.plot(ss.klist[:Nks], factor*Qeqdeltamean)
-        
+
     sqLimitTheory = np.array([np.var(ssg.ds)*NG*2 for i in range(Nks)])
     plt.plot(ss.klist[:Nks], sqLimitTheory, color="black", linestyle="dashed", linewidth=1.5, alpha=0.4)
     plt.ylabel(r'$\left\langle Q^{\rm equil}(k) \bar{\delta}_{\mathbf{r}_L} \right\rangle$')
@@ -74,6 +77,5 @@ def PQeqdeltaL(TYPE="D3", NG=100000, seeds=["7222", "4233", "221192"], subtractG
     kmin=2.*np.pi/ss.Lsub
     kmax=kmin*33
     plt.xlim(kmin, kmax)
-    
+
     plt.show()
-    

plot_ps.py
@@ -12,18 +12,18 @@ matplotlib.rcParams.update({'ytick.major.pad': 9})
 matplotlib.rcParams.update({'xtick.major.pad': 7})
 
 class SSPowerSpectra(object):
-    
+
     def __init__(self, basedir="./data/", name="normalfNL0", seed="7222", Nsubs=512, Lsub=300.):
         self.basedir=basedir
         self.name=name
         self.seed=seed
         self.Nsubs=Nsubs
-        
+
         if (Nsubs<=64):
             self.datadir=basedir+"/"+name+"/"+seed+"/pot.prim64/"
         else:
             self.datadir=basedir+"/"+name+"/"+seed+"/pot.prim512/"
-            
+
         self.filebase="pslists"
         self.fbbispec="eqbis"
         self.plt=plt
@@ -33,33 +33,39 @@ class SSPowerSpectra(object):
         self.weightedpower=None
         self.normalized=False
 
-    def iBk(self, sigmasqgL=0.0):
+    def iBk(self, sigmasqgL=0.0, variance=False):
         """
         get the integrated bispectrum (squeezed) by correlating P(k, sub) with
         the overdensity
+
+        or the correlation with the variance if the variance option is True
         """
         if self.powerspectrum==None:
             self.average_ps()
-            
+
         self.iBkmean=np.array([])
         self.iBksigma=np.array([])
         self.iBkdata=[]
-	
-	if (sigmasqgL==0.0):
-	    sigmasqL=np.var(self.ds)
-	else:
-	    sigmasqL=sigmasqgL
+
+        if (sigmasqgL==0.0):
+            sigmasqL=np.var(self.ds)
+        else:
+            sigmasqL=sigmasqgL
 
         # NEED WEIGHTED AVERAGE?
         for i in range(0, len(self.klist)):
-            iBkdat=np.array([self.powerspectra[j][i]*self.ds[j]/self.powerspectrum[i]/sigmasqL for j in range(self.Nsubs)])
+            if not(variance):
+                iBkdat=np.array([self.powerspectra[j][i]*self.ds[j]/self.powerspectrum[i]/sigmasqL for j in range(self.Nsubs)])
+            else:
+                iBkdat=np.array([self.powerspectra[j][i]*self.ds[j]*self.ds[j]/np.power(self.powerspectrum[i], 1.0)/sigmasqL for j in range(self.Nsubs)])
+
             self.iBkmean=np.append(self.iBkmean, np.mean(iBkdat))
             self.iBksigma=np.append(self.iBksigma, np.sqrt(np.var(iBkdat)))
             self.iBkdata.append(iBkdat)
-            
+
         # get <P(k) \bar{phi}> correlation per subvolume -- this does not weigh the higher k more.
         self.iBksubs = np.mean(self.iBkdata, axis=0)
-    
+
     def PPhicorr(self):
         """
         plot < PPhi \bar{Phi} > as a function of wavenumber k
@@ -73,7 +79,7 @@ class SSPowerSpectra(object):
 
     def weighted_ps(self, mfactor=1.1):
         """
-        generate weighted ps i.e. for each power spectrum compute a average 
+        generate weighted ps i.e. for each power spectrum compute a average
         weighted by k
         """
         self.weightedpower=[]
@@ -83,22 +89,22 @@ class SSPowerSpectra(object):
             nsum=np.sum(self.psdata[i][1][0:Nk])
             total=np.sum(np.array([self.psdata[i][1][j]*self.powerspectra[i][j] for j in range(Nk)]))
             self.weightedpower.append(total/nsum)
-            
+
         # also find correlation
         self.corr=[]
         for i in range(self.Nsubs):
             self.corr.append(self.ds[i]*self.weightedpower[i])
-        
+
         self.corr_mean=np.mean(self.corr)
         self.corr_sigma=np.sqrt(np.var(self.corr))
-             
+
     def average_ps(self):
         """
         get the average power spectrum from the subsamples
         """
-        
+
         self.powerspectrum=np.average(self.powerspectra, axis=0)
-    
+
     def normalize(self, reverse=False):
         """
         divide individual power spectra by the average
@@ -112,7 +118,7 @@ class SSPowerSpectra(object):
             else:
                 self.powerspectra[sub]=self.powerspectra[sub]*self.powerspectrum
                 self.normalized=False
-           
+
     def read_statistics(self):
         """
         read the power spectrum files and store appropriate data in numpy
@@ -121,19 +127,21 @@ class SSPowerSpectra(object):
         self.psdata=[]
         self.powerspectra=[]
         self.ds=[]
+        self.dsigmasq=[]
         self.dsigma=[]
         self.bsdata=[]
         self.eqbispectra=[]
         self.fNLeq=[]
-        
+
         for sub in range(self.Nsubs):
             self.psdata.append(np.load(self.datadir+self.filebase+"_"+str(sub)+".npy"))
             self.powerspectra.append(np.trim_zeros(self.psdata[-1][0][1:]))
             self.bsdata.append(np.load(self.datadir+self.fbbispec+"_"+str(sub)+".npy"))
             self.eqbispectra.append(self.bsdata[-1][0][1:len(self.powerspectra[-1])])
-            
+
             self.ds.append(np.load(self.datadir+"stat_"+str(sub)+".npy")[0])
-            self.dsigma.append(np.sqrt(np.load(self.datadir+"stat_"+str(sub)+".npy")[1]))
+            self.dsigmasq.append(np.load(self.datadir+"stat_"+str(sub)+".npy")[1])
+            self.dsigma = np.array([np.sqrt(dsq) for dsq in self.dsigmasq])
 
         self.klist=np.arange(1, len(self.powerspectra[-1]))*(2.*np.pi/self.Lsub)
         # subtract the mean ds
@@ -143,24 +151,25 @@ class SSPowerSpectra(object):
         self.fNLeqds=[]
         for i in range(len(self.eqbispectra)):
             self.fNLeqds.append(np.array([self.ds[i]*self.eqbispectra[i][j] for j in range(45)]))
-        
+
     def plot_ps(self, show=False, density=True, pcolor="r", mcolor="b", lw=0.6):
         """
         plot all subsampled power spectra in a plot
         """
-        
+
         if (density):
-            """ also read the local overdensity value and plot line colors according to
+            """ also read the local overdeOptimization of spectroscopic surveys for testing non-Gaussianity
+nsity value and plot line colors according to
             the density value, + = red, - = blue; adjust alpha accordingly
             """
             if len(self.ds)<self.Nsubs:
-                print "no density data"
+                print ("no density data")
                 return 0
             ads=np.abs(self.ds)
-	    meands=np.mean(self.ds)
+            meands=np.mean(self.ds)
             mads=np.max(ads)
             normds=np.array([ads[i]/mads/1.5 for i in range(len(ads))])
-        self.normds=normds
+            self.normds=normds
 
         for sub in range(self.Nsubs):
             #print sub
@@ -177,7 +186,7 @@ class SSPowerSpectra(object):
         #else:
         #    self.plt.ylim(500, 50000)
         #    self.plt.yscale('log')
-        
+
         self.plt.xlabel(r"$k {\rm (h/Mpc)}$")
         if (self.normalized):
             self.plt.ylabel(r"$P_{\rm subvolume}(k)/ P_{\rm avg}(k)$")
@@ -185,11 +194,11 @@ class SSPowerSpectra(object):
         else:
             self.plt.ylabel(r"$P_{\rm subvolume}(k)\; {\rm (Mpc/h)}^3$")
             self.plt.yscale('log')
-        
+
         if (show):
             self.plt.show()
-        
-    
+
+
     def plot_bs(self, show=False, density=True, pcolor="r", mcolor="b", lw=1.0):
         """
         plot eq bispectra for subsamples color coded by the average density of the subsamples
@@ -199,13 +208,14 @@ class SSPowerSpectra(object):
             the density value, + = red, - = blue; adjust alpha accordingly
             """
             if len(self.ds)<self.Nsubs:
-                print "no density data"
+                print ("no density data")
                 return 0
+
             ads=np.abs(self.ds)
-	    meands=np.mean(self.ds)
+            meands=np.mean(self.ds)
             mads=np.max(ads)
             normds=np.array([ads[i]/mads for i in range(len(ads))])
-        self.normds=normds
+            self.normds=normds
 
         for sub in range(self.Nsubs):
             #print sub
@@ -216,12 +226,11 @@ class SSPowerSpectra(object):
                     self.plt.plot(self.klist[1:-1], self.eqbispectra[sub][1:-1], color=pcolor, alpha=normds[sub], linewidth=lw)
                 else:
                     self.plt.plot(self.klist[1:-1], self.eqbispectra[sub][1:-1], color=mcolor, alpha=normds[sub], linewidth=lw)
-        
+
         self.plt.xlabel(r"$k {\rm (h/Mpc)}$")
         self.plt.ylabel(r"${\rm Q}(k)$")
         self.plt.xscale('log')
         #self.plt.yscale('log')
-        
+
         if (show):
             self.plt.show()
-        